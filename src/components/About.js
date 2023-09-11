449cf2cd5f948921fb6ba0692fe47563ed785f1d
darinetag <tagdarinefatma@gmail.com>
2023-09-11 12:47:17 +0100

    assignment DONE

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="about">
+<h2>About Me</h2>
+<p> Queen of the world with a hint of confetti to spread across the globe</p>
+<img src={image} alt="I made this"></img>
+    </div>
+      
+
+  ) 
 }
 
 export default About;
